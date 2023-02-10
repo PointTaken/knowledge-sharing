afffdb18a4b4c9daa0c38b64acdb8dc8ac2350c7
Odd Daniel Taasaasen <60235842+oddebisken@users.noreply.github.com>
2023-02-10 18:57:20 +0100

    Add files via upload

---
+++ PowerShell/Log in to Az as current user.ps1
@@ -0,0 +1,6 @@
+$azProfile = [Microsoft.Azure.Commands.Common.Authentication.Abstractions.AzureRmProfileProvider]::Instance.Profile
+$currentAzureContext = Get-AzContext
+$profileClient = New-Object Microsoft.Azure.Commands.ResourceManager.Common.RMProfileClient($azProfile)
+$token = $profileClient.AcquireAccessToken($currentAzureContext.Tenant.TenantId)
+Write-host "Logging user in to Azure AD"
+Connect-AzureAD -aadaccesstoken $token.AccessToken -accountID $currentAzureContext.Account
